aed706ebf920ac5f64d92428db34336ceb45dd0b
181250019 <181250019@smail.nju.edu.cn>
2019-05-19 12:28:46 +0800

    5.19 写出了earth planet两个类，以及以直代曲的绘制曲线的方法

---
+++ app/src/main/java/com/example/wanderingearth/Earth.java
@@ -0,0 +1,50 @@
+package com.example.wanderingearth;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.View;
+
+public class Earth extends View{
+    public Earth(Context context,float[] XDots, float[] YDots){
+        super(context);
+        this.pathXDots = XDots;
+        this.pathYDots = YDots;
+    }
+
+    private float[] pathXDots;//一系列的点用来绘制路径；
+    private float[] pathYDots;
+    //以下是一系列的getter和setter;
+    public void setXDots(float[] XDots){
+        this.pathXDots = XDots;
+    }
+    public void setYDots(float[] YDots){
+        this.pathYDots = YDots;
+    }
+    public float[] getPathXDots(){
+        return this.pathXDots;
+    }
+    public float[] getPathYDots(){
+        return this.pathYDots;
+    }
+    //改变路径的代码；
+    public float[] changeXDots(){return null;};
+    public float[] changeYDots(){return null;}
+    //绘制路径的代码
+    @Override
+    protected void onDraw(Canvas canvas){
+        Paint mypaint = new Paint();
+        Path path = new Path();
+        mypaint.setColor(Color.BLACK);
+        mypaint.setStyle(Paint.Style.STROKE);
+        mypaint.setStrokeWidth(5);
+        path.moveTo(pathXDots[0],pathYDots[0]);
+        for(int i=1; i< pathXDots.length;i++){
+            path.lineTo(pathXDots[i],pathYDots[i]);
+            canvas.drawPath(path,mypaint);
+        }
+    }
+
+}
--- app/src/main/java/com/example/wanderingearth/StartGameActivity.java
+++ app/src/main/java/com/example/wanderingearth/StartGameActivity.java
@@ -1,11 +1,9 @@
 package com.example.wanderingearth;
 
-import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 
 public class StartGameActivity extends AppCompatActivity {
     @Override
@@ -13,5 +11,12 @@ public class StartGameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//不显示状态栏的指令；
         setContentView(R.layout.startgame);
+        float[] XDots = new float[250];
+        float[] YDots = new float[250];
+        for(int i = 1;i<=250;i++){
+            XDots[i] = i;
+            YDots[i] = i*i + 2*i + 2;
+        }
+        Earth earth = new Earth(StartGameActivity.this,XDots,YDots);
     }
 }
