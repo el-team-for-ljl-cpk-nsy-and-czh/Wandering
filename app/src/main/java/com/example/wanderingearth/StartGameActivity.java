4635f9f22047f0065ee662a29f418b7038cb36ff
haha <181250081@smail.nju.edu.cn>
2019-05-22 21:56:41 +0800

    5.22 21：55 @author ljl 匹配了路径 改了切换效果 终版

--- app/src/main/java/com/example/wanderingearth/StartGameActivity.java
+++ app/src/main/java/com/example/wanderingearth/StartGameActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.transition.Fade;
 import android.transition.Slide;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.view.View;
@@ -28,9 +29,14 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import static java.lang.Math.floor;
+import static java.lang.Math.sin;
+
 public class StartGameActivity extends AppCompatActivity {
     //此处声明earth则在后续的所有方法中都可以使用earth；
     int WINDOWWIDTH,WINDOWHEIGHT;
+    final double PI=3.1415926;
+    Barrier jupiter=new Barrier();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +44,7 @@ public class StartGameActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//不显示状态栏的指令；
         setContentView(R.layout.startgame);
         getWindow().setEnterTransition(new Fade().setDuration(500));
-        getWindow().setReenterTransition(new Slide().setDuration(300)
+        getWindow().setReenterTransition(new Fade().setDuration(300)
                 .excludeChildren(R.drawable.gamebackgound,true)
                 .excludeChildren(R.drawable.cute_jupiter,true)
                 .excludeChildren(R.id.goback,true)
@@ -88,7 +94,7 @@ public class StartGameActivity extends AppCompatActivity {
         findViewById(R.id.restart).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getWindow().setExitTransition(new Slide().setDuration(300)
+                getWindow().setExitTransition(new Fade().setDuration(300)
                         .excludeChildren(R.drawable.gamebackgound,true)
                         .excludeChildren(R.drawable.cute_jupiter,true)
                         .excludeChildren(R.id.goback,true)
@@ -100,11 +106,40 @@ public class StartGameActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+
+    public void onWindowFocusChanged(boolean hasFocus){
+
+        super.onWindowFocusChanged(hasFocus);
+
+        //此处可以正常获取width、height等
+        jupiter.setMass(318);
+        TextView mass_text=findViewById(R.id.Mass_text);
+        mass_text.setText(String.valueOf(jupiter.getMass()));
+        ImageView earth_iView=findViewById(R.id.earth);
+        ImageView door_iView=findViewById(R.id.door);
+        LinearLayout layout = findViewById(R.id.LayoutInStartGame);
+        int left=earth_iView.getLeft()+earth_iView.getWidth()/2;
+        int top=earth_iView.getTop()+earth_iView.getWidth()/2;
+        Earth earth = new Earth(this);
+        float[] XDots = new float[WINDOWWIDTH-left];
+        float[] YDots = new float[WINDOWWIDTH-left];
+        for(int i = 0;i< WINDOWWIDTH-left;i++){
+            XDots[i] = i+left;
+            YDots[i] =(float) (-(jupiter.getMass()*sin((PI/(WINDOWWIDTH-left)*i))))+top;
+        }
+        earth.setXDots(XDots);
+        earth.setYDots(YDots);
+        //
+        layout.addView(earth);
+
+    }
     public void propertyMove(View v) {
         AlertDialog.Builder boom=new AlertDialog.Builder(this);
         boom.setMessage("BOOM!");
-        ImageView jupiter=findViewById(R.id.jupiter);
-        final int jupiter_radius=jupiter.getHeight()/2;
+        ImageView jupiter_iView=findViewById(R.id.jupiter);
+        final int jupiter_radius=jupiter_iView.getHeight()/2;
 
         final ImageView earth = findViewById(R.id.earth);
         final int left = earth.getLeft();
@@ -119,7 +154,7 @@ public class StartGameActivity extends AppCompatActivity {
         animator.setInterpolator(new LinearInterpolator());
 
         animator.addUpdateListener(animation -> {
-        int xEarth=earth.getLeft()+earth_radius,xJupiter=jupiter.getLeft()+jupiter_radius,yEarth=earth.getTop()+earth_radius,yJupiter=jupiter.getTop()+jupiter_radius;
+        int xEarth=earth.getLeft()+earth_radius,xJupiter=jupiter_iView.getLeft()+jupiter_radius,yEarth=earth.getTop()+earth_radius,yJupiter=jupiter_iView.getTop()+jupiter_radius;
         double distence_e_j=Math.sqrt((xEarth-xJupiter)*(xEarth-xJupiter)+(yEarth-yJupiter)*(yEarth-yJupiter));
         int current = (int) animator.getAnimatedValue();
         ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) earth.getLayoutParams();
@@ -130,7 +165,7 @@ public class StartGameActivity extends AppCompatActivity {
         else {
             layoutParams.leftMargin = left + current;
 
-            layoutParams.topMargin =(int) top - (current/10);
+            layoutParams.topMargin =(int) (top - ((jupiter.getMass()*sin((PI/(WINDOWWIDTH-left)*current)))));
 
             earth.setLayoutParams(layoutParams);
         }
@@ -139,23 +174,48 @@ public class StartGameActivity extends AppCompatActivity {
 
         animator.start();
     }
-    double a=2;
     public void plus(View v){
+        jupiter.setMass(jupiter.getMass()+1);
+        TextView mass=findViewById(R.id.Mass_text);
+        mass.setText(String.valueOf(jupiter.getMass()));
         LinearLayout layout = findViewById(R.id.LayoutInStartGame);
         layout.removeAllViews();
         ImageView earth_iView=findViewById(R.id.earth);
         int left=earth_iView.getLeft()+earth_iView.getWidth()/2;
         int top=earth_iView.getTop()+earth_iView.getWidth()/2;
-        a=a/2;
         /*
         以下是初始化earth轨迹的代码，仅供测试使用；
          */
         Earth earth = new Earth(this);
-        float[] XDots = new float[1000];
-        float[] YDots = new float[1000];
-        for(int i = 0;i< 1000;i++){
+        float[] XDots = new float[WINDOWWIDTH-left];
+        float[] YDots = new float[WINDOWWIDTH-left];
+        for(int i = 0;i< WINDOWWIDTH-left;i++){
             XDots[i] = i+left;
-            YDots[i] = (int)(a*i*i/100+top);
+            YDots[i] =(float) (-(jupiter.getMass()*sin((PI/(WINDOWWIDTH-left)*i))))+top;
+        }
+        earth.setXDots(XDots);
+        earth.setYDots(YDots);
+        //
+        layout.addView(earth);
+    }
+    public void minus(View v){
+        jupiter.setMass(jupiter.getMass()-1);
+        TextView mass=findViewById(R.id.Mass_text);
+        mass.setText(String.valueOf(jupiter.getMass()));
+        LinearLayout layout = findViewById(R.id.LayoutInStartGame);
+        layout.removeAllViews();
+        ImageView earth_iView=findViewById(R.id.earth);
+        int left=earth_iView.getLeft()+earth_iView.getWidth()/2;
+        int top=earth_iView.getTop()+earth_iView.getWidth()/2;
+        /*
+        以下是初始化earth轨迹的代码，仅供测试使用；
+         */
+        Earth earth = new Earth(this);
+        float[] XDots = new float[WINDOWWIDTH-left];
+        float[] YDots = new float[WINDOWWIDTH-left];
+        for(int i = 0;i< WINDOWWIDTH-left;i++){
+            XDots[i] = i+left;
+            YDots[i] =(float) (-(jupiter.getMass()*sin((PI/(WINDOWWIDTH-left)*i))))+top;
         }
         earth.setXDots(XDots);
         earth.setYDots(YDots);
